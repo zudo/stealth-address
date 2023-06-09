7de1e7f2991a26f8ab34ebf7cd72fa6d24799b3b
zudo <44712315+zudo@users.noreply.github.com>
2023-06-10 01:55:00 +0200

    update

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,22 @@ impl StealthAddress {
         Some(StealthAddress { s, b })
     }
 }
+impl ViewKey {
+    pub fn to_bytes(&self) -> [u8; 64] {
+        let mut bytes = [0; 64];
+        bytes[..32].copy_from_slice(self.s.as_bytes());
+        bytes[32..].copy_from_slice(self.b.compress().as_bytes());
+        bytes
+    }
+    pub fn from_slice(bytes: &[u8; 64]) -> Option<ViewKey> {
+        let s: Option<_> = Scalar::from_canonical_bytes(bytes[..32].try_into().unwrap()).into();
+        let s = s?;
+        let b = CompressedRistretto::from_slice(&bytes[32..])
+            .unwrap()
+            .decompress()?;
+        Some(ViewKey { s, b })
+    }
+}
 impl SpendKey {
     pub fn new(rng: &mut impl CryptoRngCore) -> SpendKey {
         let s = scalar::random(rng);
@@ -53,6 +69,19 @@ impl SpendKey {
             s: self.s,
             b: self.b * G,
         }
+    }
+    pub fn to_bytes(&self) -> [u8; 64] {
+        let mut bytes = [0; 64];
+        bytes[..32].copy_from_slice(self.s.as_bytes());
+        bytes[32..].copy_from_slice(self.b.as_bytes());
+        bytes
+    }
+    pub fn from_slice(bytes: &[u8; 64]) -> Option<SpendKey> {
+        let s: Option<_> = Scalar::from_canonical_bytes(bytes[..32].try_into().unwrap()).into();
+        let s = s?;
+        let b: Option<_> = Scalar::from_canonical_bytes(bytes[32..].try_into().unwrap()).into();
+        let b = b?;
+        Some(SpendKey { s, b })
     }
 }
 #[cfg(test)]
